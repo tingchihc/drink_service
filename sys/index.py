bc4acbc1ab2e5d83a5f4d73efff5ef06abfe2564
Mike <tingchih.c@gmail.com>
2025-03-25 11:25:44 +0800

    interface

--- sys/index.py
+++ sys/index.py
@@ -1,3 +1,22 @@
 import os
 import json
 
+def get_admin_info():
+    current_dir = os.getcwd()
+    ADMIN_INFO = os.path.join(current_dir, "admin", "info.json")
+    with open(ADMIN_INFO, "r") as f:
+        admin = json.load(f)
+    return admin["user"], admin["password"]
+
+def get_customer_info():
+    current_dir = os.getcwd()
+    CUSTOMER_MEMBER = os.path.join(current_dir, "customer", "member.json")
+    with open(CUSTOMER_MEMBER, "r") as f:
+        member = json.load(f)
+    
+    DATA = {}
+    for id_, info_ in member.items():
+        DATA[info_]["user"] = DATA[info_]["password"]
+    
+    return DATA
+    
